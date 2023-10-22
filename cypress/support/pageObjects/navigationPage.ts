0e064285cfe7fab3081f2c3011ba67837b419898
Mohamamd Rjoub <you@example.com>
2023-10-22 18:27:11 +0300

    ok

--- cypress/support/pageObjects/navigationPage.ts
+++ cypress/support/pageObjects/navigationPage.ts
@@ -1,21 +1,9 @@
-export class navigationPage{
-
-
-static navigateToFormLayouts(){
-
-    cy.contains('li','Forms').click();
-    cy.contains('li','Form Layouts').click();
-
-
-}
-static navigateToDatePicker(){
-
-    cy.contains('li','Datepicker').click();
-
-
-}
-
-
-
-
+export class navigationPage {
+  static navigateToFormLayouts() {
+    cy.contains("li", "Forms").click();
+    cy.contains("li", "Form Layouts").click();
+  }
+  static navigateToDatePicker() {
+    cy.contains("li", "Datepicker").click();
+  }
 }
